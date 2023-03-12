8468a801ccc753b9d28e73e6d182ebba08e82977
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-03-12 13:39:58 -0700

    add templates, verify on cpp file

---
+++ search.cpp
@@ -0,0 +1,19 @@
+#include "search.h"
+
+int main() {
+
+    int a[] = {2,3,4, 9,16, 17,42, 76, 77};
+    char ch[] = {'a', 'b', 'd', 'n', 'k'};
+
+    int size_a = sizeof(a) / sizeof(a[0]);
+    int size_ch = sizeof(ch) / sizeof(ch[0]);
+    int ls =linear_search(a, 17, size_a);
+    int lsch =linear_search(ch, 'n', size_ch);
+
+    int rbs = recursive_binary_search(a, 17, 0, size_a - 1);
+
+    int ibs = iterative_binary_search(a, 16, 0, size_a - 1);
+
+    printf("%d %d %d\n", lsch, rbs, ibs);
+    return 0;
+}
--- search.h
+++ search.h
@@ -1,18 +1,18 @@
 // Search Algorithms
 #include <stdio.h>
 
-
 // Linear search algorithm
 // Time Complexity : O(n)
 // Params : array, element to search in array, size of array
 // Returns : index if element found, -1 otherwise
-int linear_search(int arr[], size_t arr_size, int num) {
+template <typename T>
+int linear_search(T arr[], T element, size_t arr_size) {
     // empty array
     if ((int) arr_size == 0) { return -1; }
     
     for (int i = 0; i < (int) arr_size; i++){
         // if found
-        if (arr[i] == num) {
+        if (arr[i] == element) {
             return i;
         }
     }
@@ -24,20 +24,21 @@ int linear_search(int arr[], size_t arr_size, int num) {
 // Time complexity : O(log n)
 // Params : sorted array, start index, end index, element to search in array
 // Returns : index if element found, -1 otherwise
-int recursive_binary_search(int arr[], int start, int end, int num) {
+template <typename T>
+int recursive_binary_search(T arr[], T element, int start, int end) {
     if (end >= start) {
         int mid = start + (end - start) / 2;
  
         // element is present at the middle
-        if (arr[mid] == num)
+        if (arr[mid] == element)
             return mid;
  
         // element is smaller than mid, 
-        if (arr[mid] > num)
-            return recursive_binary_search(arr, start, mid - 1, num);
+        if (arr[mid] > element)
+            return recursive_binary_search(arr, element, start, mid - 1);
  
         // element can only be present
-        return recursive_binary_search(arr, mid + 1, end, num);
+        return recursive_binary_search(arr, element, mid + 1, end);
     }
  
     return -1;
@@ -47,16 +48,17 @@ int recursive_binary_search(int arr[], int start, int end, int num) {
 // Time complexity : O(log n)
 // Params : sorted array, start index, end index, element to search in array
 // Returns : index if element found, -1 otherwise
-int iterative_binary_search(int arr[], int start, int end, int num) {
+template <typename T>
+int iterative_binary_search(int arr[], T element, int start, int end) {
     while (start <= end) {
         int mid = start + (end - start) / 2;
  
         // x is present at mid
-        if (arr[mid] == num)
+        if (arr[mid] == element)
             return mid;
  
         // x greater, ignore left half
-        if (arr[mid] < num)
+        if (arr[mid] < element)
             start = mid + 1;
  
         // x is smaller, ignore right half
